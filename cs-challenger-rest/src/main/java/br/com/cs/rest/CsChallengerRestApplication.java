479913c6eede63fcffc06886241acd346de9252b
Anderson Marques <andersonmarques.ci@gmail.com>
2016-06-13 10:45:57 -0300

    Testando configuração Heroku

--- cs-challenger-rest/src/main/java/br/com/cs/rest/CsChallengerRestApplication.java
+++ cs-challenger-rest/src/main/java/br/com/cs/rest/CsChallengerRestApplication.java
@@ -2,11 +2,19 @@ package br.com.cs.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Controller
 @EnableAutoConfiguration
 public class CsChallengerRestApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsChallengerRestApplication.class, args);
