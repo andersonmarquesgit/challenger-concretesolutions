6920ac7b90688e4bdb8caff02530c46c086d0f34
Anderson Marques <andersonmarques.ci@gmail.com>
2016-06-13 11:21:23 -0300

    Removendo teste configuração Heroku

--- cs-challenger-rest/src/main/java/br/com/cs/rest/CsChallengerRestApplication.java
+++ cs-challenger-rest/src/main/java/br/com/cs/rest/CsChallengerRestApplication.java
@@ -2,19 +2,9 @@ package br.com.cs.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class CsChallengerRestApplication {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsChallengerRestApplication.class, args);
